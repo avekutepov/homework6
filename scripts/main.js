3cbd4e59294d5a1d5dd212f2769f7acd98631d78
avekutepov <kutepov_pasha@list.ru>
2022-04-02 19:23:14 +0600

    test

--- scripts/main.js
+++ scripts/main.js
@@ -12,7 +12,7 @@ const state = {
     products: null
 }
 
-function editProduct(id){
+async function editProduct(id){
     const obj = {
         title: document.getElementById('editTitle').value,
         description: document.getElementById('editDescription').value,
@@ -21,7 +21,7 @@ function editProduct(id){
         category_id: document.getElementById('editCategory_id').value,
         image: null
     }
-    fetch(`https://geektech-project.herokuapp.com/products/${id}`, {
+    await fetch(`https://geektech-project.herokuapp.com/products/${id}`, {
         method: "PUT",
         headers: {
             "Content-Type": "application/json",
@@ -35,69 +35,65 @@ function closeForm(){
     form.innerHTML =``
 }
 
-function openForm(id){
-    fetch(`https://geektech-project.herokuapp.com/products/${id}`, {
+async function openForm(id){
+    const res = await fetch(`https://geektech-project.herokuapp.com/products/${id}`, {
         method: 'GET'
-    }).then((res)=>{
-        return res.json();
-    }).then((data) => {
-        state.products = data;
-        form.innerHTML = `
-        <div class="popup__container">
-            <div class="popup__body">
-                <p>Редактируйте выбраный товар</p>
-                <p>Edit title</p>
-                <input type="text" value="${data.title}"  placeholder="name" id="editTitle">
-                <p>Edit description</p>
-                <textarea cols="30" rows="10" placeholder="description" id="editDescription">${data.description}</textarea>
-                <p>Edit price</p>
-                <input type="number" value="${data.price}" placeholder="price" id="editPrice">
-                <p>Edit stock Price</p>
-                <input type="number" value="${data.stock_price}" id="editStockPrice">
-                <p>Edit category</p>
-                <input type="number" value="${data.category_id}" placeholder="category ID" id="editCategory_id">
-                <button onclick="editProduct(${data.id})">Edit!</button>
-                <button onclick="closeForm()">Cancel editing</button>
-            </div>
-        </div>`
     })
+    const data = await res.json()
+    state.products = data;
+    form.innerHTML = `
+    <div class="popup__container">
+        <div class="popup__body">
+            <p>Редактируйте выбраный товар</p>
+            <p>Edit title</p>
+            <input type="text" value="${data.title}"  placeholder="name" id="editTitle">
+            <p>Edit description</p>
+            <textarea cols="30" rows="10" placeholder="description" id="editDescription">${data.description}</textarea>
+            <p>Edit price</p>
+            <input type="number" value="${data.price}" placeholder="price" id="editPrice">
+            <p>Edit stock Price</p>
+            <input type="number" value="${data.stock_price}" id="editStockPrice">
+            <p>Edit category</p>
+            <input type="number" value="${data.category_id}" placeholder="category ID" id="editCategory_id">
+            <button onclick="editProduct(${data.id})">Edit!</button>
+            <button onclick="closeForm()">Cancel editing</button>
+        </div>
+    </div>`
 }
 
 
 
-function deleteProduct(id) {
-    fetch(`https://geektech-project.herokuapp.com/products/${id}`, {
-        method: 'DELETE'})
+async function deleteProduct(id) {
+    await fetch(`https://geektech-project.herokuapp.com/products/${id}`, {
+        method: 'DELETE'
+    });
+    await getAllProduct()
 }
 
 
-function getAllProduct(){
+async function getAllProduct(){
     const products = document.querySelector('.products');
-    fetch(endpoints.products, {
-        method: 'GET'
-    }).then((res) => {
-        return res.json();
-        }).then((data) => {
-            state.products = data;
-            for (let i = 0; i < data.length; i++){
-                products.innerHTML += `
-                <div class="product__block">
-                    <img src="${baseUrl}${data[i].image}" alt=""/>
-                    <h3>${data[i].title}</h3>
-                    <p class="description">${data[i].description}</p>
-                    <p class="price">${data[i].price}</p>
-                    <button onclick="deleteProduct(${data[i].id})">Delete</button>
-                    <button onclick="openForm(${data[i].id})">Edit</button>
-                </div>`;
-            }
-    })
+    const res = await fetch(endpoints.products, {method: 'GET'});
+        const data = await res.json()
+        state.products = data;
+        for (let i = 0; i < data.length; i++){
+            products.innerHTML += `
+            <div class="product__block">
+                <img src="${baseUrl}${data[i].image}" alt=""/>
+                <h3>${data[i].title}</h3>
+                <p class="description">${data[i].description}</p>
+                <p class="price">${data[i].price}</p>
+                <button onclick="deleteProduct(${data[i].id})">Delete</button>
+                <button onclick="openForm(${data[i].id})">Edit</button>
+            </div>`;
+        }
 }
 
 getAllProduct();
 
 const submit = document.getElementById('submit');
 
-function addProduct(){
+async function addProduct(){
     const obj = {
         title: document.getElementById('name').value,
         description: document.getElementById('description').value,
@@ -108,18 +104,16 @@ function addProduct(){
     }
     console.log(obj)
 
-    fetch(endpoints.products, {
+    const res = await fetch(endpoints.products, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(obj)
-    }).then((res) => {
-        console.log(`
+    }); await console.log(`
         Status: ${res.status}
         Status Text: ${res.statusText}
         `)
-    })
 }
 
 submit.addEventListener('click' , addProduct)
